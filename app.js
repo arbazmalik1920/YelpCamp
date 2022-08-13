682d7e2a3fd77d6925fa6f22c8e2cdce6ad97cdb
Arbaz Malik <arbazmalik1920@gmail.com>
2022-08-13 23:40:14 +0530

    Update ports

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(process.env.PORT || 3000, function(){
-    console.log("Serving on port", this.address().port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
 
